3b55b43f86d5955ea6a261544f89a58c2a1ab3aa
Daniel Velasquez <danivelas4@gmail.com>
2025-09-17 14:46:43 -0400

    M .gitignore M variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "endpoint" { type = string }
 variable "username" { type = string }
-variable "password" { type = string }
+variable "password" { type = string, sensitive = true }
 variable "ssh_public_key" { type = string }
 variable "node" { type = string }
