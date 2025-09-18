0d457d71f74c891149abac33b099b10cfbb27ae0
Daniel Velasquez <danivelas4@gmail.com>
2025-09-18 12:31:52 -0400

    attempting to make it so that /mnt/media is a directory that is shared between the host and ubuntu VM via virtio 1. created /mnt/media in host with 777 permissions 2. made cloud-init of ubuntu VM mount itself to that directory 3. still need to make proxmox add virtiofs device to that VM M main.tf M variables.tf

--- main.tf
+++ main.tf
@@ -18,16 +18,42 @@ data "local_file" "ssh_public_key" {
   filename = var.ssh_public_key
 }
 
+resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = "proxmox"
+
+  source_raw {
+    data = <<-EOF
+    #cloud-config
+    hostname: terraform-provider-proxmox-ubuntu-vm
+    package_update: true
+    package_upgrade: true
+    users:
+      - default
+      - name: ubuntu
+        groups:
+          - sudo
+        shell: /bin/bash
+        ssh_authorized_keys:
+          - ${trimspace(data.local_file.ssh_public_key.content)}
+        sudo: ALL=(ALL) NOPASSWD:ALL
+    # create mountpoint
+    runcmd:
+      - mkdir -p /mnt/media
+      - echo "media /mnt/media virtiofs defaults 0 0" >> /etc/fstab
+      - mount -a
+    EOF
+
+    file_name = "user-data-cloud-config.yaml"
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   name = "terraform-provider-proxmox-ubuntu-vm"
   description = "Managed by Terraform"
   tags        = ["terraform", "ubuntu"]
   node_name   = var.node
-
-  clone {
-    vm_id = 9000
-    full = true
-  }
 
   cpu {
     cores = 2
@@ -36,11 +62,14 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   memory {
     dedicated = 2048
   }
-  
+
   disk {
     datastore_id = "local-lvm"
-    interface    = "scsi0"
-    size         = 8
+    import_from  = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
   }
 
   network_device {
@@ -48,18 +77,45 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     model = "virtio"
   }
 
+  # this initialization block works because:
+  # under the hood, bpg/proxmox created a VM template and stored it in the proxmox "local" storage
+  # this template is configured so that when a VM using this template boots for the first time,
+  # there will be a special cloud-init drive in it. this allows us to pass data into the VM
+  # like SSH keys, hostname, network config, etc
   initialization {
     datastore_id = "local-lvm"
 
     ip_config {
       ipv4 {
-        address = "dhcp"
+        address = "10.0.0.84/24"
+        gateway = "10.0.0.1"
       }
     }
 
-    user_account {
-      username = "ubuntu"
-      keys     = [trimspace(data.local_file.ssh_public_key.content)]
-    }
+    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_config.id
   }
+}
+
+# here, we are telling bpg/proxmox to create a VM template that is used by our ubuntu VM.
+# we are specifying where that template should be stored.
+# we are also specifying the specific cloud image that should be used in this VM template
+resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = var.node
+  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  # need to rename the file to *.qcow2 to indicate the actual file format for import
+  file_name = "noble-server-cloudimg-amd64.qcow2"
+}
+
+# Map the host directory to a virtiofs resource
+resource "proxmox_virtual_environment_hardware_mapping_dir" "media_mount" {
+  name     = "media_mount"
+  comment  = "media bind mount"
+  map = [
+    {
+      node = "proxmox"
+      path = "/mnt/media"
+    },
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
 variable "endpoint" { type = string }
 variable "username" { type = string }
-variable "password" { type = string, sensitive = true }
+variable "password" {
+  type = string
+  sensitive = true
+}
 variable "ssh_public_key" { type = string }
 variable "node" { type = string }
